a80153e950f214896b2e3b60ec02b65990b97fbd
Skyrdow <102250934+Skyrdow@users.noreply.github.com>
2022-09-14 00:27:24 -0300

    Cambios en el script y comentarios

--- pixel.rkt
+++ pixel.rkt
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #lang racket
 
 (require "otras_funciones.rkt")
@@ -183,3 +184,190 @@
 (define (pixhex->string pix) (if (false? pix)
                                  "#------"
                                  (getPixVal pix)))
+=======
+#lang racket
+
+(require "otras_funciones.rkt")
+(provide (all-defined-out))
+
+
+;;; TDA pixel
+
+;; selectores genericos de pixeles
+; Entrada: Pixel
+; Salida: String
+; Descripción: retorna el tipo del pixel
+(define (getPixType pix) (first pix))
+; Entrada: Pixel
+; Salida: Entero
+; Descripción: retorna la coordenada X del pixel
+(define (getPixPosX pix) (second pix))
+; Entrada: Pixel
+; Salida: Entero
+; Descripción: retorna la coordenada Y del pixel
+(define (getPixPosY pix) (third pix))
+; Entrada: Pixel
+; Salida: Entero | Lista de Enteros | String
+; Descripción: retorna el valor de color que almacena el pixel
+(define (getPixVal pix) (fourth pix))
+; Entrada: Pixel
+; Salida: Entero
+; Descripción: retorna la profundidad del pixel
+(define (getPixDepth pix)(fifth pix))
+
+;; modificadores genericos de pixeles
+; Entrada: Pixel X Valor (String)
+; Salida: Pixel
+; Descripción: retorna un pixel idéntico, con el valor de tipo modificado
+(define (setPixType pix val) (list val (getPixPosX pix) (getPixPosY pix) (getPixVal pix) (getPixDepth pix)))
+; Entrada: Pixel X Valor (Entero)
+; Salida: Pixel
+; Descripción: retorna un pixel idéntico, con el valor de la coordenada x modificado
+(define (setPixPosX pix val) (list (getPixType pix) val (getPixPosY pix) (getPixVal pix) (getPixDepth pix)))
+; Entrada: Pixel X Valor (Entero)
+; Salida: Pixel
+; Descripción: retorna un pixel idéntico, con el valor de la coordenada y modificado
+(define (setPixPosY pix val) (list (getPixType pix) (getPixPosX pix) val (getPixVal pix) (getPixDepth pix)))
+; Entrada: Pixel X Valor (Entero | Lista de 3 Enteros | String)
+; Salida: Pixel
+; Descripción: retorna un pixel idéntico, con el valor de color modificado
+(define (setPixVal pix val) (list (getPixType pix) (getPixPosX pix) (getPixPosY pix) val (getPixDepth pix)))
+; Entrada: Pixel X Valor (Entero)
+; Salida: Pixel
+; Descripción: retorna un pixel idéntico, con el valor de profundidad modificado
+(define (setPixDepth pix val) (list (getPixType pix) (getPixPosX pix) (getPixPosY pix) (getPixVal pix) val))
+; Entrada: Pixel X Valor x X Valor y
+; Salida: Pixel
+; Descripción: retorna un pixel idéntico, con ambas coordenadas x e y modificadas
+(define (setPixXY pix x y) (list (getPixType pix) x y (getPixVal pix) (getPixDepth pix)))
+
+;; otras funciones
+; Entrada: String X Pixel
+; Salida: Booleano
+; Descripción: función generica que compara el tipo de un pixel
+(define (compType type pix) (equal? type (getPixType pix)))
+; Entrada: Pixel
+; Salida: Pixel
+; Descripción: intercambia coordenadas x e y de un pixel
+(define (swapPixXY pix) (setPixXY  pix (getPixPosY pix) (getPixPosX pix)))
+; Entrada: Pixrgb
+; Salida: Pixhex
+; Descripción: transforma los valores (r g b) de un pixrgb, los transforma y los retorna en un pixel hexadecimal "#RRGGBB"
+(define (pixrgb->pixhex pix) (setPixType (setPixVal pix (string-upcase (string-append "#" (int->hex (getR pix)) (int->hex (getG pix)) (int->hex (getB pix))))) "pixhex-d"))
+
+  
+;; TDA pixbit:
+; Entrada: x (Entero) X y (Entero) X bit (Entero, 0 o 1) X profundidad (Entero, de 0 a 255)
+; Salida: Pixbit
+; Descripción: constructor de pixbit, los valores de color deben ser restringidos a lo que indica en la documentación, y la posición del pixel debe estar dentro de la imagen respectiva
+(define (pixbit-d x y bit depth) (list "pixbit-d" x y bit depth))
+; Entrada: Pixbit
+; Salida: Bit (Entero)
+; Descripción: selector del bit de pixbit
+(define (getBit pixbit) (getPixVal pixbit))
+; Entrada: Pixbit | Elemento Basado en Lista
+; Salida: Booleano
+; Descripción: pertenencia de pixbit
+(define (pixbit? pix) (compType "pixbit-d" pix))
+; Entrada: Pixbit
+; Salida: String
+; Descripción: transforma el valor de color de un pixbit a string, si entra #F retorna "#------" (el pixel no existe)
+(define (pixbit->string pix) (if (false? pix)
+                                 "#------"
+                                 (if (equal? 0 (getBit pix))
+                                     "0"
+                                     "1")))
+
+; Entrada: Pixbit
+; Salida: Pixbit
+; Descripción: invierte el valor del bit
+(define (invertColorBit pix) (setPixVal pix (- 1 (getPixVal pix))))
+
+
+;; TDA pixrgb:
+; Entrada: x (Entero) X y (Entero) X r (Entero, de 0 a 255) X g (Entero, de 0 a 255) X b (Entero, de 0 a 255) X profundidad (Entero, de 0 a 255)
+; Salida: pixrgb
+; Descripción: constructor de pixrgb, los valores de color deben ser restringidos a lo que indica en la documentación, y la posición del pixel debe estar dentro de la imagen respectiva
+(define (pixrgb-d x y r g b depth) (list "pixrgb-d" x y (list r g b) depth))
+; Entrada: Pixrgb
+; Salida: Lista de Enteros
+; Descripción: selector de la lista que almacena los valores RGB
+(define (getRgb pixrgb) (getPixVal pixrgb))
+; Entrada: Pixrgb
+; Salida: Entero
+; Descripción: retorna el valor R del pixel
+(define (getR pix) (first (getRgb pix)))
+; Entrada: Pixrgb
+; Salida: Entero
+; Descripción: retorna el valor G del pixel
+(define (getG pix) (second (getRgb pix)))
+; Entrada: Pixrgb
+; Salida: Entero
+; Descripción: retorna el valor B del pixel
+(define (getB pix) (third (getRgb pix)))
+; Entrada: Pixrgb X valor (Entero)
+; Salida: Pixrgb
+; Descripción: modificador del valor R del pixel
+(define (setR pix valR) (setPixVal pix (list valR (getG pix) (getB pix))))
+; Entrada: Pixrgb X valor (Entero)
+; Salida: Pixrgb
+; Descripción: modificador del valor G del pixel
+(define (setG pix valG) (setPixVal pix (list (getR pix) valG (getB pix))))
+; Entrada: Pixrgb X valor (Entero)
+; Salida: Pixrgb
+; Descripción: modificador del valor B del pixel
+(define (setB pix valB) (setPixVal pix (list (getR pix) (getG pix) valB)))
+
+; Entrada: Pixrgb
+; Salida: Booleano
+; Descripción: pertenencia de pixrgb
+(define (pixrgb? pix) (compType "pixrgb-d" pix))
+; Entrada: Pixrgb
+; Salida: String
+; Descripción: retorna el valor RGB del pixel, transformandolo a pixhex primero, si entra #F retorna "#------" (el pixel no existe)
+(define (pixrgb->string pix) (if (false? pix)
+                                 "#------"
+                                 (pixhex->string (pixrgb->pixhex pix))))
+
+; Entrada: Pixrgb
+; Salida: Pixrgb
+; Descripción: invierte el color representado por los valores RGB
+(define (invertColorRGB pix) (setPixVal pix (list (- 255 (getR pix)) (- 255 (getG pix)) (- 255 (getB pix)))))
+
+; Entrada: Función getR|getG|getB X Función setR|setG|setB X Función  X Pixrgb
+; Salida: pixrgb
+; Tipo de Recursión: 
+; Algoritmo/Estrategia: 
+; Descripción: adj channel uso: (edit  (adjustChannel getR setR incCh) img)
+(define adjustChannel (lambda (f1 f2 f3) (lambda (pix) (f2 pix (f3 (f1 pix))))))
+; Entrada: Entero
+; Salida: Entero
+; Descripción: aumenta en 1 el valor ingresado
+(define (incCh val) (+ 1 val))
+; Entrada: Entero
+; Salida: Entero
+; Descripción: disminuye en 1 el valor ingresado
+(define (decCh val) (- val 1))
+
+
+
+;; TDA pixhex:
+; Entrada: x (Entero) X y (Entero) X hex (String, de formato "#RRGGBB") X profundidad (Entero, de 0 a 255)
+; Salida: Pixhex
+; Descripción: constructor de pixhex, los valores de color deben ser restringidos a lo que indica en la documentación, y la posición del pixel debe estar dentro de la imagen respectiva
+(define (pixhex-d x y hex depth) (list "pixhex-d" x y hex depth))
+; Entrada: Pixhex
+; Salida: String
+; Descripción: selector del hex de pixhex
+(define (getHex pixhex) (getPixVal pixhex))
+; Entrada: Pixhex
+; Salida: Booleano
+; Descripción: pertenencia de pixhex
+(define (pixhex? pix) (compType "pixhex-d" pix))
+; Entrada: Pixhex | Falso
+; Salida: String
+; Descripción: retorna el valor de color que almacena pixhex, por el contrario, si entra #F retorna "#------" (el pixel no existe)
+(define (pixhex->string pix) (if (false? pix)
+                                 "#------"
+                                 (getPixVal pix)))
+>>>>>>> b33208d23321fa98279aa705bc52e536fdf36ffd
--- script.rkt
+++ script.rkt
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #lang racket
 (require "pixel.rkt")
 (require "image.rkt")
@@ -351,3 +352,358 @@
 ;las siguientes comparaciones deberían arrojar #f
 (equal? img25 img2)
 (equal? img26 img1)
+=======
+#lang racket
+(require "pixel.rkt")
+(require "image.rkt")
+
+(define imgextra1 (image 2 2
+                    (pixrgb-d 0 0 60 1 1 10)
+                    (pixrgb-d 0 1 2 80 2 20)
+                    (pixrgb-d 1 0 5 3 60 10)
+                    (pixrgb-d 1 1 145 90 123  1)))
+
+(define imgextra2 (image 2 2
+                  (pixbit-d 0 0 0 10)
+                  (pixbit-d 0 1 1 20)
+                  (pixbit-d 1 0 1 10)
+                  (pixbit-d 1 1 0 255)))
+
+(define imgextra3 (imgRGB->imgHex imgextra1))
+
+(define imgextra4 (image 2 3
+                  (pixbit-d 0 0 0 10)
+                  (pixbit-d 0 1 1 20)
+                  (pixbit-d 1 0 1 10)
+                  (pixbit-d 0 2 0 10)
+                  (pixbit-d 1 2 1 10)
+                  (pixbit-d 1 1 0 255)))
+
+(define imgextra5 (image 3 1
+                  (pixrgb-d 0 0 10 20 30 10)
+                  (pixrgb-d 1 0 40 50 60 20)
+                  (pixrgb-d 2 0 80 90 100 10)))
+
+;imprimir una representación string de las imagenes
+(display "Funciones de prueba con los 3 ejemplos extra requeridos\nImg1 (RGB): \n")
+(display (image->string imgextra1 pixrgb->string))
+
+(display "\nimgextra2 (BIT): \n")
+(display (image->string imgextra2 pixbit->string))
+
+(display "\nimgextra3 (HEX): \n")
+(display (image->string imgextra3 pixhex->string))
+(display "\nimgextra4 (BIT): \n")
+(display (image->string imgextra4 pixbit->string))
+(display "\nimgextra5 (RGB): \n")
+(display (image->string imgextra5 pixrgb->string))
+
+
+(display "\n## Pertenencias:\n\n# Bitmap: \n")
+(display "\nimgextra1: \n")
+(bitmap? imgextra1)
+(display "\nimgextra2: \n")
+(bitmap? imgextra2)
+(display "\nimgextra3: \n")
+(bitmap? imgextra3)
+
+(display "\n# Pixmap: \n")
+(display "\nimgextra1: \n")
+(pixmap? imgextra1)
+(display "\nimgextra2: \n")
+(pixmap? imgextra2)
+(display "\nimgextra3: \n")
+(pixmap? imgextra3)
+
+(display "\n# Hexmap: \n")
+(display "\nimgextra1: \n")
+(hexmap? imgextra1)
+(display "\nimgextra2: \n")
+(hexmap? imgextra2)
+(display "\nimgextra3: \n")
+(hexmap? imgextra3)
+
+(display "\n# Compressed?: \n")
+(display "\nimgextra1: \n")
+(compressed? imgextra1)
+(display "\nimgextra2: \n")
+(compressed? imgextra2)
+(display "\nimgextra3: \n")
+(compressed? imgextra3)
+
+(display "\n## Flips: \n")
+(display "\n# FlipH: \n")
+(display "\nimgextra1: \n")
+(display (image->string (flipH imgextra1) pixrgb->string))
+(display "\nimgextra2: \n")
+(display (image->string (flipH imgextra2) pixbit->string))
+(display "\nimgextra3: \n")
+(display (image->string (flipH imgextra3) pixhex->string))
+
+(display "\n# FlipV: \n")
+(display "\nimgextra1: \n")
+(display (image->string (flipV imgextra1) pixrgb->string))
+(display "\nimgextra2: \n")
+(display (image->string (flipV imgextra2) pixbit->string))
+(display "\nimgextra3: \n")
+(display (image->string (flipV imgextra3) pixhex->string))
+
+(display "\n## Crop: \n")
+(display "\n# imgextra1, p1=0,0 p2=0,0 \n")
+(display (image->string (crop imgextra1 0 0 0 0) pixrgb->string))
+(display "\n# imgextra2, p1=0,0 p2=0,1 \n")
+(display (image->string (crop imgextra2 0 0 0 1) pixbit->string))
+(display "\n# imgextra3, p1=0,1 p2=1,1 \n")
+(display (image->string (crop imgextra3 0 1 1 1) pixhex->string))
+
+(display "\n## imgRGB->imgHex:\n")
+(display "\n# imgextra1 -> hex \n")
+(display (image->string (imgRGB->imgHex imgextra1) pixhex->string))
+(display "\n# Crop imgextra1, p1=0,0 p2=1,0 -> hex \n")
+(display (image->string (imgRGB->imgHex (crop imgextra1 0 0 1 0)) pixhex->string))
+(display "\n# imgextra5 -> hex \n")
+(display (image->string (imgRGB->imgHex imgextra5) pixhex->string))
+
+(display "\n\n## histogram: \n")
+(display "\n# imgextra1: \n")
+(histogram imgextra1)
+(display "\n# imgextra2: \n")
+(histogram imgextra2)
+(display "\n# imgextra3: \n")
+(histogram imgextra3)
+
+(display "\n\n## rotate90: \n")
+(display "\n# imgextra1: \n")
+(display (image->string (rotate90 imgextra1) pixrgb->string))
+(display "\n# imgextra2: \n")
+(display (image->string (rotate90 imgextra2) pixbit->string))
+(display "\n# imgextra3: \n")
+(display (image->string (rotate90 imgextra3) pixhex->string))
+
+(display "\n\n## compress: \n")
+(display "\n# imgextra1: \n")
+(compress imgextra1)
+(display "\n# imgextra2: \n")
+(compress imgextra2)
+(display "\n# imgextra3: \n")
+(compress imgextra3)
+(display "\n# Compressed?: \n")
+(display "\n# imgextra1: \n")
+(compressed? (compress imgextra1))
+(display "\n# imgextra2: \n")
+(compressed? (compress imgextra2))
+(display "\n# imgextra3: \n")
+(compressed? (compress imgextra3))
+
+(define imgextra8 (compress imgextra1))
+(define imgextra9 (compress imgextra2))
+(define imgextra10 (compress imgextra3))
+
+(display "\n\n## edit: \n")
+(display "\n\n# invertColorBit: \n")
+(display "\n# imgextra2: \n")
+(display (image->string (edit invertColorBit imgextra2) pixbit->string))
+(display "\n# crop imgextra2 p1=0,0 p2=0,1: \n")
+(display (image->string (edit invertColorBit (crop imgextra2 0 0 0 1)) pixbit->string))
+(display "\n# imgextra4: \n")
+(display (image->string (edit invertColorBit imgextra4) pixbit->string))
+
+(display "\n\n# invertColorRgb: \n")
+(display "\n# imgextra1: \n")
+(display (image->string (edit invertColorRGB imgextra1) pixrgb->string))
+(display "\n# crop imgextra1 p1=0,0 p2=0,1: \n")
+(display (image->string (edit invertColorRGB (crop imgextra1 0 0 0 1)) pixrgb->string))
+(display "\n# imgextra5: \n")
+(display (image->string (edit invertColorRGB imgextra5) pixrgb->string))
+
+(display "\n\n# adjustChannel: \n")
+(display "\n# imgextra1, getR setR decCh: \n")
+(display (image->string (edit (adjustChannel getR setR decCh) imgextra1) pixrgb->string))
+(display "\n# imgextra1, getG setG decCh: \n")
+(display (image->string (edit (adjustChannel getG setG decCh) imgextra1) pixrgb->string))
+(display "\n# imgextra1, getB setB decCh: \n")
+(display (image->string (edit (adjustChannel getB setB decCh) imgextra1) pixrgb->string))
+
+(display "\n\n## image->string: \n")
+(display "\nimgextra1 (BIT): \n")
+(display (image->string imgextra1 pixrgb->string))
+(display "\nimgextra2 (BIT): \n")
+(display (image->string imgextra2 pixbit->string))
+(display "\nimgextra3 (HEX): \n")
+(display (image->string imgextra3 pixhex->string))
+
+(display "\n\n## depthLayers: \n")
+(display "\nimgextra1: \n")
+(depthLayers imgextra1)
+(display "\nimgextra2: \n")
+(depthLayers imgextra2)
+(display "\nimgextra3: \n")
+(depthLayers imgextra3)
+
+
+(display "\n\n## decompress: (se comprimieron primero, luego se descomprimieron) \n")
+(display "\nimgextra1: \n")
+(decompress imgextra8)
+(display "\nimgextra2: \n")
+(decompress imgextra9)
+(display "\nimgextra3: \n")
+(decompress imgextra10)
+
+(display "\n## FIN DE LAS PRUEBAS EXTRA. INICIO DEL  \"SCRIPT  BÁSICO DE PRUEBAS\"  \n\n")
+
+;img1
+;Creación de una imagen de 2 x 2 del tipo pixmap
+(define img1 (image 2 2
+                  (pixrgb-d 0 0 255 0 0 10)
+                  (pixrgb-d 0 1 0 255 0 20)
+                  (pixrgb-d 1 0 0 0 255 10)
+                  (pixrgb-d 1 1 255 255 255  1)
+ ))
+
+;img2
+;Creación de una imagen de 2 x 2 del tipo bitmap
+(define img2 (image 2 2
+                  (pixbit-d 0 0 0 10)
+                  (pixbit-d 0 1 1 20)
+                  (pixbit-d 1 0 1 10)
+                  (pixbit-d 1 1 0 255)
+ ))
+
+(define img3 (imgRGB->imgHex img1))
+
+
+;imprimir una representación string de la imagen
+(display (image->string img1 pixrgb->string))
+
+;imprimir una representación string de la imagen
+(display (image->string img2 pixbit->string))
+
+(bitmap? img1) ; la respuesta debería ser #f
+(bitmap? img2)  ; la respuesta debería ser #t
+(bitmap? img3)  ; la respuesta debería ser #f
+
+(pixmap? img1) ; la respuesta debería ser #t
+(pixmap? img2)  ; la respuesta debería ser #f
+(pixmap? img3)  ; la respuesta debería ser #f
+
+(hexmap? img1) ; la respuesta debería ser #f
+(hexmap? img2)  ; la respuesta debería ser #f
+(hexmap? img3)  ; la respuesta debería ser #t
+
+(compressed? img1) ; la respuesta debería ser #f
+(compressed? img2) ; la respuesta debería ser #f
+(compressed? img3) ; la respuesta debería ser #f
+
+(flipH img1)
+(flipH img2)
+(flipH img3)
+
+(flipV img1)
+(flipV img2)
+(flipV img3)
+
+(define img4 (crop img1 0 0 0 0)) ; debería retornar una imágen con un pixel
+(define img5 (crop img2 0 0 0 1)) ; debería retornar una imágen con dos pixeles
+(define img6 (crop img1 0 1 1 1)) ; debería retornar una imágen con dos pixeles
+(define img7 (crop img2 0 0 1 1)) ; debería retornar la misma imagen
+
+(histogram img1)
+(histogram img2)
+(histogram img3)
+(histogram img4)
+(histogram img5)
+(histogram img6)
+(histogram img7)
+
+(define img18 (rotate90 img1))
+(define img19 (rotate90 img2))
+(define img20 (rotate90 img3))
+(define img21 (rotate90 img4))
+(define img22 (rotate90 img5))
+(define img23 (rotate90 img6))
+(define img24 (rotate90 img7))
+
+(define img8 (compress img1))
+(define img9 (compress img2))
+(define img10 (compress img3))
+(define img11 (compress img4))
+(define img12 (compress img5))
+(define img13 (compress img6))
+(define img14 (compress img7))
+
+(compressed? img8)  ; la respuesta debería ser #t
+(compressed? img9)  ; la respuesta debería ser #t
+(compressed? img10)  ; la respuesta debería ser #t
+(compressed? img11)  ; la respuesta debería ser #t
+(compressed? img12)  ; la respuesta debería ser #t
+(compressed? img13)  ; la respuesta debería ser #t
+(compressed? img14)  ; la respuesta debería ser #t
+
+(define img15 (edit invertColorBit img2))
+(define img16 (edit invertColorRGB img1))
+
+;se asume que las funciones de ajuste de canal están implementadas. 
+;Puede cambiarlas por otras en su script de pruebas si así lo prefiere 
+;(define img15 (edit (adjustChannel getR setR incCh) img1))
+;(define img16 (edit (adjustChannel getG setG incCh) img1))
+(define img17 (edit (adjustChannel getB setB incCh) img1))
+
+;imágenes no comprimidas
+(display (image->string img1 pixrgb->string))
+(display (image->string img2 pixbit->string))
+(display (image->string img3 pixhex->string))
+(display (image->string img4 pixrgb->string))
+(display (image->string img5 pixbit->string))
+;(display (image->string img6 pixhex->string))
+;(display (image->string img7 pixrgb->string))
+
+;imagenes comprimidas, podrían internamente descomprimirlas para convertir a string ;(opcional)
+(display (image->string img8 pixrgb->string))
+(display (image->string img9 pixbit->string))
+(display (image->string img10 pixhex->string)) 
+(display (image->string img11 pixrgb->string))
+(display  (image->string img12 pixbit->string))
+(display (image->string img13 pixrgb->string))
+(display (image->string img14 pixbit->string))
+
+;imágenes no comprimidas
+;(display (image->string img15 pixrgb->string))
+(display (image->string img16 pixrgb->string))
+(display (image->string img17 pixrgb->string))
+(display (image->string img18 pixrgb->string))
+(display (image->string img19 pixbit->string))
+(display (image->string img20 pixhex->string))
+(display (image->string img21 pixrgb->string))
+(display (image->string img22 pixbit->string))
+(display (image->string img23 pixrgb->string))
+(display (image->string img24 pixbit->string))
+
+(depthLayers img1)
+(depthLayers img2)
+(depthLayers img3)
+(depthLayers img4)
+(depthLayers img5)
+(depthLayers img6)
+(depthLayers img7)
+
+(define img25 (decompress img8))
+(define img26 (decompress img9))
+(define img27 (decompress img10))
+(define img29 (decompress img11))
+(define img30 (decompress img12))
+(define img31 (decompress img13))
+(define img32 (decompress img14))
+
+;las siguientes comparaciones deberían arrojar #t
+(equal? img25 img1)
+(equal? img26 img2)
+(equal? img27 img3)
+;(equal? img28 img4)
+(equal? img29 img5)
+(equal? img30 img6)
+(equal? img31 img7)
+
+;las siguientes comparaciones deberían arrojar #f
+(equal? img25 img2)
+(equal? img26 img1)
+>>>>>>> b33208d23321fa98279aa705bc52e536fdf36ffd
